42c0ccffd484926dc5a4392a944e20808a8dbd7b
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-04-15 18:11:24 -0500

    Added a class and functions for seed generation

--- encryption.py
+++ encryption.py
@@ -1,18 +1,16 @@
 import random
+from toBinaryOne import toSingleBinaryByte
+import math
 
 
 alphabet = [  'a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z']
 
-i = 0
+class kryptor():
+    def __init__(self):
+        pass
+    def seedMath(seed):
+        #some function
+        return math.sin(seed)
 
-#builds a string for each character in the alphabet based off some key 
-#WIP
-while i < 26:
-    
-    
-
-
-
-
-    i += 1
-
+    def genSeed(message):
+        seed = toSingleBinaryByte(message)
