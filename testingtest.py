37b619834787aa7572a25df49cc91afd92673801
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-04-15 18:11:55 -0500

    Added test for seed generation

--- testingtest.py
+++ testingtest.py
@@ -1,7 +1,12 @@
 from toBinaryOne import toSingleBinaryByte
 import unittest
+from encryption import kryptor
 
 class TestBinary(unittest.TestCase):
     def test_tobinary(self):
         result = toSingleBinaryByte("andrew stinks12345784654606478917309271-4327041-2356216!@#$%&*()+^_")
         self.assertEqual(result, 21889554)
+    def test_kryptorr(self):
+        testKryptor = kryptor
+        testSeedValue = 0.01745240643 
+        self.assertEqual(testKryptor.seedMath(1), testSeedValue)
