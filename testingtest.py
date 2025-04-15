ba06b3042334549f7398fa3639d5282a108faae4
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-04-15 18:12:30 -0500

    Changed seedGen test from test_kryptor to test_seedGen

--- testingtest.py
+++ testingtest.py
@@ -6,7 +6,7 @@ class TestBinary(unittest.TestCase):
     def test_tobinary(self):
         result = toSingleBinaryByte("andrew stinks12345784654606478917309271-4327041-2356216!@#$%&*()+^_")
         self.assertEqual(result, 21889554)
-    def test_kryptorr(self):
+    def test_seedGen(self):
         testKryptor = kryptor
         testSeedValue = 0.01745240643 
         self.assertEqual(testKryptor.seedMath(1), testSeedValue)
