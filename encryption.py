fbf3a8401e9414eeb0644215582f87166ad85a7b
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-04-22 10:39:17 -0500

    Added the full cipher functionality

--- encryption.py
+++ encryption.py
@@ -9,7 +9,7 @@ class seeder():
     def seedMath(self, input):
         random.seed(input)
         delta = random.randint(1, 10)
-        return float((math.sqrt(42*math.pow(math.pi, math.e)*input))/math.sqrt(math.e))/delta
+        return float((math.sqrt(42*math.pow(math.pi, math.e)*input))/math.sqrt(math.e))/delta #use this function to generate a large number
 
     def genSeed(self, message):
         seed = toSingleBinaryByte(message)
@@ -18,15 +18,24 @@ class seeder():
 
 class enCrypt():
     def __init__(self):
-        self.alphabet = [  'a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z']
+        self.alphabet = [    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ':', ';', '<', '=', '>', '?', '@', '_', '^']
         self.cipher = {}
         self.encryptedMessage = []
     def genCipher(self, message):
-        random.seed(seeder.genSeed(self, message))
-        for i in self.alphabet:
-            self.cipher[i] = random.randint(1, len(self.alphabet))
-        return self.cipher
-
-
-testDict = {"a": 1}
-testDict2 = {"a": 2}
+        seedGen = seeder() # add a seeder object for seeding of the random function
+        random.seed(seedGen.genSeed(message)) # seed the random function
+        for i in self.alphabet: # for each character in the alphabet array
+            characterLength = random.randint(3, 7) #generate the random character length 
+            characterValue = ""
+            while characterLength > 0: #generate the character 
+                characterValue = characterValue + self.alphabet[random.randint(0, len(self.alphabet)-1)]
+                characterLength -= 1
+            self.cipher[i] = characterValue # add the character to the dictionary 
+         
+        return self.cipher # return the cipher
