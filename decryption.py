9deb5020a642b3d4ec916f289ed4b37ae137a8b4
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-04-22 23:48:56 -0500

    Added the genCipher and some getters

---
+++ decryption.py
@@ -0,0 +1,32 @@
+import random
+import math
+
+
+class deCrypt():
+    def __init__(self, key):
+        self.alphabet = [    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ':', ';', '<', '=', '>', '?', '@', '_', '^']
+        self.cipher = {}
+        self.key = key
+        self.delim = ""
+    def genCipher(self):
+        random.seed(self.key)
+        for i in self.alphabet: # for each character in the alphabet array
+            characterLength = random.randint(3, 7) #generate the random character length 
+            characterValue = ""
+            while characterLength > 0: #generate the character 
+                characterValue = characterValue + self.alphabet[random.randint(0, len(self.alphabet)-1)]
+                characterLength -= 1
+            self.cipher[i] = characterValue # add the character to the dictionary 
+        self.delim += self.alphabet[random.randint(0,len(self.alphabet)-1)]
+        return self.cipher # return the cipher
+    def getKey(self):
+        return self.key
+    
+    def getDelim(self):
+        return self.delim
