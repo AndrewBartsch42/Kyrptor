f3b631284eb7708c84bc4f79263e5481d0902ae0
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-04-29 13:21:45 -0500

    Added routing to the different pages stil WIP

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from flask import Flask
-
+from flask import Flask, render_template, request #https://www.youtube.com/watch?v=RuSH-DEmwEg flask tutorial
+from encryption import enCrypt
+from decryption import deCrypt
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<form action=\"/action_page.php\"><input type=\"text\" id=\"fname\" name=\"fname\"></form>"
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        link = request.form.get('link')
+        if link == "encrypt":
+            return encryption()
+        elif link == "decrypt":
+            return decryption()
+    return render_template('index.html')
+def encryption():
+    encryptor = enCrypt()
+    if request.method == "POST":
+        msg = str(request.form.get("MSG"))
+        encryptedMSG = encryptor.encryptMessage(msg)
+        return render_template("output.html", text=encryptedMSG, key=encryptor.getSeed())
+    return render_template('encrypt.html')
+def decryption():
+    if request.method == "POST":
+        encryptedMsg = str(request.form.get("encryptedMSG"))
+        seed = request.form.get("key", float)
+        decryptor = deCrypt(seed)
+        msg = decryptor.deCrypt(encryptedMsg)
+        return render_template("output.html", text=msg)
+    return render_template('decrypt.html')
+if __name__ == '__main__':
+    app.run(debug=True)
